4d3fffee03812612bfa736df6ba6f71a471063df
Nish-008 <farihazaman.nishat@gmail.com>
2022-12-25 14:13:56 +0600

    change in CORS permsission

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose
     app.use(express.json());
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+      res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
